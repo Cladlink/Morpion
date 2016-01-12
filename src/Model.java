296d9fbccda35b9d2c250c6790211ab3d5e291b5
cladlink <michael.boutboul87@gmail.com>
2016-01-12 09:42:29 +0100

    V1

--- src/Model.java
+++ src/Model.java
@@ -24,7 +24,7 @@ public class Model
     {
         boolean victoire = false;
         if (plateau[i][j].getIcon() == plateau[i][(j+1)%plateau[i].length].getIcon() && plateau[i][j].getIcon() == plateau[i][Math.abs((j-1)%plateau[i].length)].getIcon()
-         || plateau[i][j].getIcon() == plateau[(i+1)%plateau.length][j] && plateau[i][j].getIcon() == plateau[Math.abs((i-1)%plateau[i].length)][j].getIcon())
+         || plateau[i][j].getIcon() == plateau[(i+1)%plateau.length][j].getIcon() && plateau[i][j].getIcon() == plateau[Math.abs((i-1)%plateau[i].length)][j].getIcon())
         {
             System.out.println(Math.abs((j-1)%plateau[i].length));
             victoire = true;
